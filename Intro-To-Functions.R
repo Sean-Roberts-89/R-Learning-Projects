eba8da8905d89baa20e727eddf544f3c6eaeea1c
Sean-Roberts-89 <125078607+Sean-Roberts-89@users.noreply.github.com>
2023-02-18 13:01:55 -0600

    Beginning Class Material....
    Still more to go, have not completed.

--- Intro-To-Functions.R
+++ Intro-To-Functions.R
@@ -1,1 +1,22 @@
-# test
+# Introduction to functions ####
+# This can help us with reusable code
+# Using https://www.youtube.com/watch?v=p8tAQx7ijXE
+
+sum(c(1,2,3)) # sum of vector 1,2,3
+
+fuction_name <- function(inputs) {
+  output_value <- do_something(inputs)
+return(output_value)
+  } #inputs = arguments
+
+#Create a function for calculating the length of a shrub from the L*W*H
+
+calc_shrub_vol <- function(length, width, height) {
+  area <- length * width 
+  volume <- area * height
+  return(volume)
+} # This is all about defining the function. 
+
+
+calc_shrub_vol(0.8, 1.6, 2.0)
+shrub_vol <- calc_shrub_vol(0.8, 1.6, 2.0)
